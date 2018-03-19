00814280a0fc1b643842a1f2c3c33d67fde4bc45
Kevin Lai <kevin@Kevins-MacBook-Pro.local>
2018-03-19 20:43:03 +1100

    changed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import NameForm from './components/prompt/NameForm';
 import PropViewer from './components/prompt/PropViewer';
+import PageNotFound from './components/PageNotFound';
 
 const App = () => {
 
@@ -19,6 +20,7 @@ const App = () => {
                         <Route path='/' component={Home} exact />
                         <Route path='/prompt' component={NameForm} />
                         <Route path='/props' component={PropViewer} />
+                        <Route component={PageNotFound} />
                     </switch>
 
                 </div>
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,13 +1,30 @@
 import React from "react";
-// import { Link } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
+
 const Sidebar = () => {
+
+    const links = [
+        {name: 'Color', url: '/color'},
+        {name: 'Prompt', url: '/prompt'},
+        {name: 'Prop Viewer', url: '/props'},
+        {name: '404 Error', url: '/404Error'},
+    ];
+
+    let linksComponents = links.map((link, index) => {
+        return (
+            <li key={index} className={'nav'}>
+                <NavLink className={'navLink'} activeClassName={'activeNavLink'}
+                         to={link.url} exact>{link.name}</NavLink>
+            </li>
+        );
+    });
 
     return (
         <div className={'leftNavContainer'}>
-            <NavLink to='/' activeClassName='activeNavLink' className='navLink' exact>Home</NavLink ><br />
-            <NavLink to='/prompt' activeClassName='activeNavLink' className='navLink'>Prompt</NavLink ><br />
-            <NavLink to='/props' className='navLink'>Props</NavLink ><br />
+            <ul>
+                <li className='nav'><NavLink className={'navLink'} activeClassName={'activeNavLink'} to='/' exact>Home</NavLink></li>
+                {linksComponents}
+            </ul>
         </div>
     );
 };
