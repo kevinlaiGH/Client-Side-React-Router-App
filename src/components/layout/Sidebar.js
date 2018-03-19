38440b57d5b4a4ff5a8c3bb18e14cc7bccced57c
Kevin Lai <kevin@Kevins-MacBook-Pro.local>
2018-03-19 18:56:25 +1100

    changed NavLInk

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-
+// import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 const Sidebar = () => {
 
-    return (
-        <div className={'leftNavContainer'}>
-            <Link to='/' className='navLink'>Home</Link ><br />
-            <Link to='/prompt' className='navLink'>Prompt</Link ><br />
-            <Link to='/props' className='navLink'>Props</Link ><br />
-        </div>
-    );
+    return (
+        <div className={'leftNavContainer'}>
+            <NavLink to='/' activeClassName='activeNavLink' className='navLink' exact>Home</NavLink ><br />
+            <NavLink to='/prompt' activeClassName='activeNavLink' className='navLink'>Prompt</NavLink ><br />
+            <NavLink to='/props' className='navLink'>Props</NavLink ><br />
+        </div>
+    );
 };
 
 export default Sidebar;
